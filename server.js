23cc56ff4bc1dcc486bd0b62789d0a436bdb1d36
Emiliano <mnnnkosh@gmail.com>
2022-01-06 11:02:58 -0300

    [MODULE] Using Routing to Build Multiple Pages

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
+
+const sessionsRouter = require('./src/routers/sessionsRouter');
 const debug = require('debug')('server');
 const server = express();
 const path = require('path');
-
 const PORT = process.env.PORT || 3000;
+
 server.use(morgan('tiny'));
 server.use(express.static(path.join(__dirname, '/public')));
 
 server.set('views', './src/views');
 server.set('view engine', 'ejs');
+
+server.use('/sessions', sessionsRouter);
 
 server.get('/', (req, res) => {
   res.render('index', { title: 'MONJS', data: ['a', 'b', 'c'] });
